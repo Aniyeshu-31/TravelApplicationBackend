f1ffe74a04c0f6d4cbcaefc5d4118c7b251d20a2
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-04-14 13:52:52 +0530

    Updated Backend!!!

--- app.js
+++ app.js
@@ -16,9 +16,23 @@ const corsOption = {
   credentials: true,
 }
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://transcendent-sundae-2bb9db.netlify.app','https://travel-booking-application-ten.vercel.app'], // Allow local dev and deployed frontend
+  origin: [
+    'http://localhost:3000',
+    'https://transcendent-sundae-2bb9db.netlify.app',
+    'https://travel-booking-application-ten.vercel.app',
+  ], // Allow local dev and deployed frontend
   credentials: true, // Allow cookies and credentials
-};
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Access-Control-Allow-Origin',
+    'Content-Type',
+    'Authorization',
+    'Origin',
+    'X-Requested-With',
+    'Accept',
+  ],
+  exposedHeaders: ['Access-Control-Allow-Credentials'],
+}
 app.use(cors(corsOptions))
 // Allow frontend at localhost:3000
 // middleware
