ab197f36b8d2e8d102b1b6f13791a5877e8bcfd3
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-04-14 16:29:28 +0530

    updated!!

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,12 @@ import { LoginUser, registerUser } from '../controllers/authController.js';
 const router = express.Router();
 router.post('/register',registerUser);
 router.post('/login',LoginUser);
-
+router.get('/me', verifyToken, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'User is authenticated',
+    data: req.user, // This depends on your `verifyToken` middleware
+  })
+})
 
 export default router;
