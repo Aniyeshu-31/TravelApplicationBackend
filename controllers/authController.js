a76fb8fe8da3b8c2ba9af62da4e6491c680401d1
Aniyeshu <202152304@iiit.vadodara.ac.in>
2025-04-14 14:38:27 +0530

    updated domain!!!

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,6 +43,7 @@ export const LoginUser = async(req,res,next)=>{
            secure: true,
            sameSite: 'none',
            maxAge: 15 * 24 * 60 * 60 * 1000,
+           domain: 'https://travelapplicationbackend.onrender.com',
          })
          .status(200)
          .json({
